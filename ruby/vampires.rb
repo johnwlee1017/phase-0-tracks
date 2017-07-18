4d49d9cd9d54a50bb4a7b4454c12d59ab5dccb0b
John Lee <johnwlee1017@gmail.com>
2017-07-17 17:34:12 -0700

    add code to determine right_age and fake_name

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,27 @@
 puts "What is your name?"
 employee_name = gets.chomp
 
+# determine if employee name is known vampire name
+if employee_name == "Drake Cula"
+  fake_name = true
+elsif employee_name == "Tu Fang"
+  fake_name = true
+else
+  fake_name = false
+end
+
 puts "How old are you?"
 employee_age = gets.chomp.to_i
 
 puts "What year were you born?"
 employee_bornyear = gets.chomp.to_i
+
+# determine right_age
+if employee_age == 2017 - employee_bornyear
+  right_age = true
+else
+  right_age = false
+end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
