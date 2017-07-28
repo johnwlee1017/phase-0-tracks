98f03c76e427a4afb609f442374a8ffeea385d80
John Lee <johnwlee1017@gmail.com>
2017-07-28 10:00:15 -0700

    add characteristics and behaviors to puppy class as comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES (class)------------------------
+# Puppy
+
+# CHARACTERISTICS (attributes)----------------
+# Legs: 4
+# Fur coat: varies
+# Pet: yes
+# Name: varies
+# Color: varies
+
+# BEHAVIOR (methods)-----------------------
+# Lick
+# Run
+# Jump
