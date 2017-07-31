9151bbcbb666736da121fb36a22cdfd65ca3e07f
John Lee <johnwlee1017@gmail.com>
2017-07-31 07:35:35 -0700

    complete release 1, 2, & 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,14 @@
 class Santa
+  attr_reader :reindeer_ranking, :age
+  attr_accessor :gender, :ethnicity
+
+  def initialize(gender,ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
 
   def speak
     puts "Ho, ho, ho! Happy Holidays!"
@@ -8,13 +18,43 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
-  def initialize
-    puts "Initializing Santa instance ..."
+  def celebrate_birthday
+    p @age + 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.insert(-1,reindeer_name)
+    p @reindeer_ranking
   end
 
 end
 
-john = Santa.new
+santa = Santa.new("male", "korean")
 
-john.speak
-john.eat_milk_and_cookies("oreo")
+santa.speak
+santa.eat_milk_and_cookies("oreo")
+santa.get_mad_at("Vixen")
+santa.gender = "random gender"
+santa.celebrate_birthday
+p santa.ethnicity
+
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas[1].ethnicity
+
+
+
+
+
+
+
+
