47a798bbabde6c2eb68e1d3a9a1942ae48bef7de
John Lee <johnwlee1017@gmail.com>
2017-08-03 16:33:26 -0700

    add test for all methods

--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -2,10 +2,18 @@ require_relative 'wordgame'
 
 describe WordGame do
   let(:wordgame) { WordGame.new }
+  let(:correct_word) { ["t","e","s","t"] }
+  #used attributes, to use variables in the instance methods
 
-  it "calculates number of guesses" do
-    expect(wordgame.guesses_allowed("word")).to eq 4
+  it "converts correct word to dashes (as array)" do
+    expect(wordgame.correct_word_to_dashes).to eq #["_", "_", "_", "_"]
   end
 
+  it "displays correct letter from user input" do
+    expect(wordgame.display_correct_letter).to eq #
+  end
 
+  it "displays congrats message if all letters matched" do
+    expect(wordgame.complete_match).to eq #
+  end
 end
