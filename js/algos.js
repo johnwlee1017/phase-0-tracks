faf12a06e6e80ac0590b282fbde2d0a7c7fa2dec
John Lee <johnwlee1017@gmail.com>
2017-08-09 18:18:10 -0700

    add sort and find word functions

---
+++ js/algos.js
@@ -0,0 +1,51 @@
+// take array (of strings) and find length of each element, and create new array of lengths.
+// with lengths array, add if statement for having largest number to the front. compare each element(index) with each other.
+// create object to have key(word) and value(length) pairs.
+
+
+var word_array = ["word", "longestword", "otherword", "randomword", "wordword"]
+var length_array = []
+var words = {}
+
+for (var i = 0; i < word_array.length; i++) {
+  length_array.push(word_array[i].length);
+}
+
+console.log(length_array)
+
+for (var i = 0; i < word_array.length; i++) {
+  words[word_array[i]] = length_array[i];
+}
+
+console.log(words)
+
+function sort(length_array) {
+  var array = length_array;
+
+  for (var i = 0; i < array.length; i++) {
+    for (var ii = 0; ii < array.length - i - 1; ii++) {
+      if (array[ii] > array[ii+1]) {
+        var prev_element = array[ii];
+        array[ii] = array[ii+1];
+        array[ii+1] = prev_element;
+      }
+    }
+  }
+  return length_array;
+}
+
+console.log(sort(length_array))
+
+function find_word(words, length_array) {
+  var object = words;
+  var value = length_array[length_array.length-1];
+
+  return Object.keys(object).find(key => object[key] === value);
+}
+
+console.log(find_word(words, length_array))
+
+
+
+
+
