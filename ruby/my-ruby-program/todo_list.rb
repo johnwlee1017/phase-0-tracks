49feea4698032a65111a67d1153b69512ead61b1
John Lee <johnwlee1017@gmail.com>
2017-08-15 10:06:36 -0700

    create todo_list.db and todo_list.rb

---
+++ ruby/my-ruby-program/todo_list.rb
@@ -0,0 +1,17 @@
+# create todo list that asks user what tasks need to be done and then allows user to view the list and also add/remove items from the list.
+#
+# create menu, asking what would you like to do (give options and ask to select the number):
+# 1 - add item to todo list (displays current list and asks to user to add to list)
+# 2 - remove item from todo list (displays current list and asks which item(s) to remove)
+# 3 - view todo list (displays current list)
+#
+# create database (todo_list) and add table (tasks) with user input.
+#
+# sections:
+# 1 - add item to todo list
+# if user selects 1, takes user to add item todo list.
+# display current list (create method to retrieve data from tasks table from db)
+# ask user "Please add the task(s) to the list (and enter "done" when finished)."
+# create method to add to items to the tasks table.
+# if user is finished and enters "done", it takes user back to the menu
+
